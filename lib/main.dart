f15dafad403ea6388731361b6ac32cd23a331dca
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-06-16 23:05:27 -0400

    added more styling

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
         title: 'Namer App',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightGreenAccent),
         ),
         home: MyHomePage(),
       ),
@@ -39,6 +39,7 @@ class MyHomePage extends StatelessWidget {
     var pair = appState.current;
     return Scaffold(
       body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text('Startup name:'),
           BigCard(pair: pair),
@@ -62,10 +63,19 @@ class BigCard extends StatelessWidget {
   final WordPair pair;
   @override
   Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final style = theme.textTheme.displayMedium!.copyWith(
+      color: theme.colorScheme.onPrimary,
+    );
     return Card(
+      color: theme.colorScheme.primary,
       child: Padding(
         padding: const EdgeInsets.all(20),
-        child: Text(pair.asLowerCase),
+        child: Text(
+          pair.asLowerCase,
+          style: style,
+          semanticsLabel: "${pair.first} ${pair.second}",
+        ),
       ),
     );
   }
